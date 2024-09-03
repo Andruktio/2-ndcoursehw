82bd99ec43af9292e3c6c6fb6987368d62db9215
Andructio <mr.andreyster@yandex.ru>
2024-09-03 23:54:56 +0500

    Домашнее задание 2

---
+++ hw-2.js
@@ -0,0 +1,52 @@
+/* Задание 1 */
+let a = 10;
+alert(a);
+a=20;
+alert(a);
+
+/* Задание 2 */
+const firstIphone=2007;
+alert(`Год выхода первого iPhone ${firstIphone}`);
+
+/* Задание 3 */
+const creatorJs='Брендан Эйк';
+alert(`Создатель языка JS это ${creatorJs}`)
+
+/* Задание 4 */
+let x=10;
+let y=2;
+alert(`Сумма ${x+y}, разность ${x-y}, произведение ${x*y}, частное ${x/y}`);
+
+/* Задание 5 */
+let result=2**5;
+alert(`2 в степени 5 = ${result}`);
+
+/* Задание 6 */
+let q = 9;
+let b= 2;
+alert(`Остаток от деления переменной a на b равен ${q%b}`);
+
+/* Задание 7 */
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+/* Задание 8 */
+let age=prompt('Сколько вам лет?')
+alert(`Вам ${age} лет`)
+
+/* Задание 9 */
+const user={
+    name:'Олег',
+    age:33,
+    isAdmin: false
+}
+
+/* Задание 10 */
+let yourName=prompt('Как тебя зовут?')
+alert(`Привет, ${yourName}!`)
